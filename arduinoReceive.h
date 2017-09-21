651794a873f6f405f243e191b7466324a98d3840
camillejr <kamilazdybal@gmail.com>
2017-09-21 21:43:47 +0200

    Working on PDF

--- arduinoReceive.h
+++ arduinoReceive.h
@@ -7,16 +7,18 @@
 
 class arduino
 {
-private:
-	FILE* arduinoFile;
-	std::vector<int> durations;
 
-public:
+	public:
 	void start();
 	void stop();
 	void read();
 	void clear();
 	std::string output();
+
+	private:
+	FILE* arduinoFile;
+	std::vector<int> durations;
+
 };
 
 #endif
