bf9e32b872879e456d7f9fa12e6f0a8454ad0a26
camillejr <kamilazdybal@gmail.com>
2017-09-23 19:40:44 +0200

    Description of the Arduino code

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -8,38 +8,42 @@ boolean prevSignal = false;
 int timeDuration = 0;
 int timePrev = 0;
 
-void setup() {
-  
+void setup()
+{
+
   Serial.begin(9600);
-  
+
   // Calibration
-  for(int i=0; i<10; ++i) {
-   sensorValue += analogRead(analogInPin);
-   delay(100);
+  for(int i=0; i<10; ++i)
+  {
+    sensorValue += analogRead(analogInPin);
+    delay(100);
   }
   sensorMean = sensorValue / 10;
   sensorThres = sensorMean + 20;
 }
 
 void loop() {
-  
+
   sensorValue = analogRead(analogInPin);
- 
+
   // Signal changing from Low to High
-  if (sensorValue > sensorThres && !prevSignal) {
-  timeDuration = millis() - timePrev;
-  timePrev = millis();
-  Serial.print("\nL ");
-  Serial.print(timeDuration);
-  prevSignal = true;
+  if (sensorValue > sensorThres && !prevSignal)
+  {
+    timeDuration = millis() - timePrev;
+    timePrev = millis();
+    Serial.print("\nL ");
+    Serial.print(timeDuration);
+    prevSignal = true;
   }
-  
+
   // Signal changing from High to Low
-  if (sensorValue - sensorMean < 10 && prevSignal) {
-  timeDuration = millis() - timePrev;
-  timePrev = millis();
-  Serial.print("\nH ");
-  Serial.print(timeDuration);
-  prevSignal = false;
+  if (sensorValue - sensorMean < 10 && prevSignal)
+  {
+    timeDuration = millis() - timePrev;
+    timePrev = millis();
+    Serial.print("\nH ");
+    Serial.print(timeDuration);
+    prevSignal = false;
   }
 }
